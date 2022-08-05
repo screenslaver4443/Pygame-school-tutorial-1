cda84057318dcbb10ec34ec60c31a11dc3e5c5a7
Nikolai <npesudovs@outlook.com>
2022-08-05 10:04:41 +0930

    Added Events removed redundancy

--- main.py
+++ main.py
@@ -33,13 +33,16 @@ while not done: # Main game loop
         if event.type == pygame.QUIT:
             print("User asked to quit")
             done = True
+        if event.type == pygame.KEYDOWN:
+            print('User pressed a key')
+        if event.type == pygame.KEYUP:
+            print('User let go of a key')
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("User clicked the mouse")
     ##### Game logic #####
 
     ##### Drawing code #####
     screen.fill(WHITE) #Reset the screen
     pygame.display.flip() #Update the screen with changes
-    clock.tick (60) #Limit to 60 frames per second 
-    
-    pygame.display.update()
-    
+    clock.tick (60) #Limit to 60 frames per second     
 pygame.quit()
