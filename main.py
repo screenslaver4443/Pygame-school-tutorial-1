03780c25492613e7df34abd2fe9158bc14926843
Nikolai <npesudovs@outlook.com>
2022-08-11 13:56:54 +0930

    Turned arrow into crazy shape

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ while not done:  # Main game loop
     # pygame.draw.line(screen, YELLOW, [462, 190], [300, 300], 10)
     # pygame.draw.line(screen, YELLOW, [462, 415], [300, 300], 10)
     
-    ## arrow ##
-    pygame.draw.polygon(screen, BLACK, [(0,400), (200, 200), (400, 400), (200, 100), (0, 400)], 10 )
+    ## Crazy Shape ##
+    pygame.draw.polygon(screen, BLACK, [(0,600), (230, 80), (100, 23), (420, 69), (89, 400)], 100 )
     
     pygame.display.flip()  # Update the screen with changes
 
